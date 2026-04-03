1a750924e2f24e18cf26cfa952e5def3e6d7ffe4
Dale Zak <dalezak@gmail.com>
2026-04-03 08:12:36 -0600

    Fixed Nuxt 4 issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'node:url';
+import { resolve, dirname } from 'node:path';
 import packageJson from './package.json';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 export default defineNuxtConfig({
   srcDir: 'app',
+  compatibilityDate: '2026-04-03',
   ssr: true,
   debug: false,
   devtools: { 
     enabled: true 
   },
   extends: [
-    'github:dalezak/nuxt-ionic',
-    'github:dalezak/nuxt-supabase'
+    '../nuxt-ionic',
+    '../nuxt-supabase'
   ],
   modules: [],
+  supabase: {
+    types: resolve(__dirname, '../nuxt-supabase/app/types/database.types.ts')
+  },
   runtimeConfig: {
     public: {
       url: process.env.APP_URL,
